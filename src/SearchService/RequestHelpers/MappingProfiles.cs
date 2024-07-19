5f1a9020d74d81c11a542f8b13c4e185dd56d6d7
Carlos Dominguez Puig <carlos.dom13@hotmail.com>
2024-07-19 21:39:42 +0200

    auction commit

---
+++ src/SearchService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,11 @@
+using AutoMapper;
+using Contracts;
+using SearchService.Models;
+
+namespace SearchService.RequestHelpers;
+
+public class MappingProfiles : Profile {
+    protected MappingProfiles() {
+        CreateMap<AuctionCreated, Item>();
+    }
+}
